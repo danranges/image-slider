ea6fb5c40e834039db6bb9e8a3b5d96e4ac12466
Dan Ranges <danranges@gmail.com>
2021-11-10 21:33:53 +0100

    Add carousel looping

--- main.js
+++ main.js
@@ -4,11 +4,32 @@ let i = 1;
 slider.style.transform = `translateX(${-i * 600}px)`;
 
 document.querySelector('[data-button-next]').addEventListener('click', () => {
-  i += 1;
-  slider.style.transform = `translateX(${-i * 600}px)`;
+  slider.style.transition = 'transform 300ms ease-in-out';
+
+  if (i <= 5) {
+    i += 1;
+    slider.style.transform = `translateX(${-i * 600}px)`;
+  }
 });
 
 document.querySelector('[data-button-prev]').addEventListener('click', () => {
-  i -= 1;
+  slider.style.transition = 'transform 300ms ease-in-out';
+
+  if (i >= 1) {
+    i -= 1;
+    slider.style.transform = `translateX(${-i * 600}px)`;
+  }
+});
+
+slider.addEventListener('transitionend', () => {
+  slider.style.transition = 'none';
+
+  if (i === 0) {
+    i = 5;
+  }
+  if (i === 6) {
+    i = 1;
+  }
+
   slider.style.transform = `translateX(${-i * 600}px)`;
 });
